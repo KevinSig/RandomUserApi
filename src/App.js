4cf4aeeba78b0f632c91171c70a92f7da67d38f8
Amir Sigari <amir@fureyfs.com>
2021-03-29 20:09:12 -0400

    first and last

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useFetch = url => {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    const [item] = data.results
+
+    console.log(item)
+    setData(item)
+    setLoading(false)
+  }, [])
+
+  return { data, loading }
 }
 
-export default App;
+function App() {
+  const [count, setCount] = useState(0)
+
+  const { data, loading } = useFetch("https://randomuser.me/api")
+
+  useEffect(() => {
+    document.title = `${count}`
+  }, [count])
+
+  return (
+    <div className='App'>
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      {loading ? <div>loading</div> : <div>{data.name.first}</div>}
+    </div>
+  )
+}
+
+export default App
